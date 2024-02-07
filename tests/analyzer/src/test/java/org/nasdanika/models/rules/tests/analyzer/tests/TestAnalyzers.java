eb11bb778374766c3d9d96d27df07ecc467c43bb
Pavel <Pavel.Vlasov@nasdanika.org>
2024-02-07 08:49:00 -0500

    Cleanup, test of loading pom.xml from GitLab

--- tests/analyzer/src/test/java/org/nasdanika/models/rules/tests/analyzer/tests/TestAnalyzers.java
+++ tests/analyzer/src/test/java/org/nasdanika/models/rules/tests/analyzer/tests/TestAnalyzers.java
@@ -2,9 +2,17 @@ package org.nasdanika.models.rules.tests.analyzer.tests;
 
 import java.util.ServiceLoader;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.junit.jupiter.api.Test;
+import org.nasdanika.common.Util;
+import org.nasdanika.models.gitlab.util.GitLabURIHandler;
+import org.nasdanika.models.maven.Project;
+import org.nasdanika.models.maven.util.MavenResourceFactory;
 import org.nasdanika.models.rules.Inspector;
-import org.nasdanika.models.rules.RuleSet;
 
 public class TestAnalyzers {
 	
@@ -15,5 +23,37 @@ public class TestAnalyzers {
 			System.out.println(rsf);
 		}
 	}
+		
+	@Test
+	public void testLoadPomXmlFromGitLab() {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", new MavenResourceFactory());
+		
+		String accessToken = System.getenv("GITLAB_ACCESS_TOKEN");
+		if (!Util.isBlank(accessToken)) {	
+			try (GitLabURIHandler gitLabURIHandler = new GitLabURIHandler("https://gitlab.com/", accessToken)) {
+				URI pomURI = URI.createHierarchicalURI(
+						GitLabURIHandler.GITLAB_URI_SCHEME, 
+						"48523784", 
+						null, 
+						new String[] { "main", "pom.xml" }, 
+						null, 
+						"/");
+				
+				resourceSet.getURIConverter().getURIHandlers().add(0, gitLabURIHandler);
+				
+				Resource pomXmlResource = resourceSet.getResource(pomURI, true);
+				for (EObject root: pomXmlResource.getContents()) {
+					Project project = (Project) root;
+					System.out.println(project.getGroupId());
+					System.out.println(project.getArtifactId());
+					System.out.println(project.getVersion());
+					System.out.println(project.getName());			
+					System.out.println(project.getDescription());			
+				}		
+			}
+		}
+		
+	}	
 				
 }
