6ae2d3a5378da7298eb0c61f8da72d7b5012048d
Pavel <Pavel.Vlasov@nasdanika.org>
2024-02-08 08:23:48 -0500

    Testing retrieval of trees from GitLab

--- tests/analyzer/src/main/java/module-info.java
+++ tests/analyzer/src/main/java/module-info.java
@@ -3,6 +3,7 @@ import org.nasdanika.models.rules.Inspector;
 module org.nasdanika.models.rules.tests.analyzer{
 		
 	requires transitive org.nasdanika.models.rules.tests.inspectors;
+	requires org.eclipse.emf.ecore.xmi;
 	
 	exports org.nasdanika.models.rules.tests.analyzer;
 	
--- tests/analyzer/src/test/java/org/nasdanika/models/rules/tests/analyzer/tests/TestAnalyzers.java
+++ tests/analyzer/src/test/java/org/nasdanika/models/rules/tests/analyzer/tests/TestAnalyzers.java
@@ -1,5 +1,6 @@
 package org.nasdanika.models.rules.tests.analyzer.tests;
 
+import java.io.IOException;
 import java.util.ServiceLoader;
 
 import org.eclipse.emf.common.util.URI;
@@ -7,8 +8,10 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.junit.jupiter.api.Test;
 import org.nasdanika.common.Util;
+import org.nasdanika.models.gitlab.util.GitLabApiProvider;
 import org.nasdanika.models.gitlab.util.GitLabURIHandler;
 import org.nasdanika.models.maven.Project;
 import org.nasdanika.models.maven.util.MavenResourceFactory;
@@ -31,7 +34,8 @@ public class TestAnalyzers {
 		
 		String accessToken = System.getenv("GITLAB_ACCESS_TOKEN");
 		if (!Util.isBlank(accessToken)) {	
-			try (GitLabURIHandler gitLabURIHandler = new GitLabURIHandler("https://gitlab.com/", accessToken)) {
+			try (GitLabApiProvider gitLabApiProvider = new GitLabApiProvider("https://gitlab.com/", accessToken)) {
+				GitLabURIHandler gitLabURIHandler = new GitLabURIHandler(gitLabApiProvider.getGitLabApi());				
 				URI pomURI = URI.createHierarchicalURI(
 						GitLabURIHandler.GITLAB_URI_SCHEME, 
 						"48523784", 
@@ -55,5 +59,31 @@ public class TestAnalyzers {
 		}
 		
 	}	
+		
+	@Test
+	void testLoadTreeResource() throws IOException {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());		
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", new MavenResourceFactory());
+		
+		String accessToken = System.getenv("GITLAB_ACCESS_TOKEN");
+		if (!Util.isBlank(accessToken)) {	
+			try (GitLabApiProvider gitLabApiProvider = new GitLabApiProvider("https://gitlab.com/", accessToken)) {
+				GitLabURIHandler gitLabURIHandler = new GitLabURIHandler(gitLabApiProvider.getGitLabApi());				
+				URI resourceURI = URI.createHierarchicalURI(
+						GitLabURIHandler.GITLAB_URI_SCHEME, 
+						"48523784", 
+						null, 
+						new String[] { "main", "src", "" }, 
+						null, 
+						null);
+				
+				resourceSet.getURIConverter().getURIHandlers().add(0, gitLabURIHandler);
+				
+				Resource resource = resourceSet.getResource(resourceURI, true);
+				resource.save(System.out, null);
+			}
+		}
+	}
 				
 }
